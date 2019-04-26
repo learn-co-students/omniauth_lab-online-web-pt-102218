09a941fc31317676ad5f644069f7f33ed87f793d
github username <github email address>
2019-04-26 18:35:12 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#home'
+  resources :users, :sessions
+  get '/auth/facebook/callback' => 'sessions#create'
 end
